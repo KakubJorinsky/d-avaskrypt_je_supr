e11be1b78c41cf177cddb5e38c3a54031441ba1d
Kakub Jořínský <jakub.korinsky@student.souepl.cz>
2025-01-06 09:11:03 +0100

    s_panem_bockem

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
-let i = 1;
-function Add(){
-    let input = document.getElementById("input");
-    let nazev = document.getElementById("nazev");
-    let output = document.getElementById("output");
-   
-    output.innerHTML += "<br>" + i + ". " + nazev.value;
-    i++;
-    console.log("Hodnota v inputu je: ", input.value);
+let zak = [
+    {
+        Jmeno: "Petr",
+        Prijmeni: "Kostka",
+        Vek: 15,
+        Rocnik: 1,
+        Obor: "IT",
+        Typ: "Maturitní",
+        Typ_studia: "Prezenční"
+    },
+    {
+        Jmeno: "Klement",
+        Prijmeni: "Gottwald",
+        Vek: 15,
+        Rocnik: 3,
+        Obor: "IT",
+        Typ: "Maturitní",
+        Typ_studia: "Dálkové"
+    }
+];
 
-    document.getElementById('myImage').src= input.value;
-    console.log(document.getElementById("myImage"));
-}
+document.getElementById("output").innerHTML += zak[0].Jmeno + " " + zak[0].Prijmeni + ", " + zak[0].Vek + " let, " + zak[0].Rocnik + ". ročník" + ", Obor: " + zak[0].Obor
+document.getElementById("output").innerHTML += "<br>"
+document.getElementById("output").innerHTML += zak[1].Jmeno + " " + zak[1].Prijmeni + ", " + zak[1].Vek + " let, " + zak[1].Rocnik + ". ročník" + ", Obor: " + zak[1].Obor
 
-
+console.log(zak[0].Jmeno, zak[0].Prijmeni + ",", zak[0].Vek,"let,", zak[0].Rocnik + ". ročník", ", Obor", zak[0].Obor,);
+console.log(zak[1].Jmeno, zak[1].Prijmeni + ",", zak[1].Vek,"let,", zak[1].Rocnik + ". ročník", ", Obor", zak[1].Obor,);
