889288a396f82241ac5dfa4948a169eb9397d1bc
Kakub Jořínský <jakub.korinsky@student.souepl.cz>
2025-01-20 08:13:13 +0100

    a

--- js/script.js
+++ js/script.js
@@ -19,9 +19,9 @@ let zak = [
     }
 ];
 
-document.getElementById("output").innerHTML += zak[0].Jmeno + " " + zak[0].Prijmeni + ", " + zak[0].Vek + " let, " + zak[0].Rocnik + ". ročník" + ", Obor: " + zak[0].Obor
-document.getElementById("output").innerHTML += "<br>"
-document.getElementById("output").innerHTML += zak[1].Jmeno + " " + zak[1].Prijmeni + ", " + zak[1].Vek + " let, " + zak[1].Rocnik + ". ročník" + ", Obor: " + zak[1].Obor
+document.getElementById("output").innerHTML += zak[0].Jmeno + " " + zak[0].Prijmeni + ", " + zak[0].Vek + " let, " + zak[0].Rocnik + ". ročník" + ", Obor: " + zak[0].Obor;
+document.getElementById("output").innerHTML += "<br>";
+document.getElementById("output").innerHTML += zak[1].Jmeno + " " + zak[1].Prijmeni + ", " + zak[1].Vek + " let, " + zak[1].Rocnik + ". ročník" + ", Obor: " + zak[1].Obor;
 
-console.log(zak[0].Jmeno, zak[0].Prijmeni + ",", zak[0].Vek,"let,", zak[0].Rocnik + ". ročník", ", Obor", zak[0].Obor,);
-console.log(zak[1].Jmeno, zak[1].Prijmeni + ",", zak[1].Vek,"let,", zak[1].Rocnik + ". ročník", ", Obor", zak[1].Obor,);
+console.log(zak[0].Jmeno, zak[0].Prijmeni + ",", zak[0].Vek,"let,", zak[0].Rocnik + ". ročník", ", Obor", zak[0].Obor);
+console.log(zak[1].Jmeno, zak[1].Prijmeni + ",", zak[1].Vek,"let,", zak[1].Rocnik + ". ročník", ", Obor", zak[1].Obor);
